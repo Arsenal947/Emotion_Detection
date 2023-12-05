d9a156b87d376e1bc37cdce1c3ef589cb9e7921e
Arsalan Mustafa <96394315+Arsenal947@users.noreply.github.com>
2023-12-05 06:43:27 +0530

    Update main.py

--- service/main.py
+++ service/main.py
@@ -5,7 +5,7 @@ from service.api.api import main_router
 app = FastAPI(project_name= "Emotion Detection")
 app.include_router(main_router)
 
-model_path = 'service/vit_onnx.onnx'
+model_path = './service/vit_onnx.onnx'
 providers = ['CPUExecutionProvider']
 m_q = rt.InferenceSession(model_path, providers = providers)
 
